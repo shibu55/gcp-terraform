60e435f8ac781377954a3003d05d8fbfecdcd8b2
shibu55 <segusuigao@gmail.com>
2023-12-22 15:20:34 +0900

    initial commit

---
+++ main.tf
@@ -0,0 +1,22 @@
+variable "PROJECT_KEY_PATH" {}
+
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file(var.PROJECT_KEY_PATH)
+
+  project = "Kubernetes Project"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
